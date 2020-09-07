f6d383da127ce4bef1d955bbe2eb3df2c84b6aa7
Travis Arnold <ftntravis@gmail.com>
2020-09-07 16:06:09 -0700

    add types for Overrides

--- packages/jsx-ui/src/Overrides.tsx
+++ packages/jsx-ui/src/Overrides.tsx
@@ -4,14 +4,14 @@ import { getInstance } from './utils'
 
 const OverridesContext = React.createContext([])
 
-export function useOverrideProps<Props>(instance, props) {
-  const OverridesStack = React.useContext(OverridesContext)
+export function useOverrideProps<Props>(component, props) {
+  const overridesStack = React.useContext(OverridesContext)
   let modifiedProps = {} as Props
-  OverridesStack.forEach(Overrides => {
-    Overrides.forEach(override => {
+  overridesStack.forEach(overrides => {
+    overrides.forEach(override => {
       const components = override.slice(0, -1).map(getInstance)
       const overrideProps = override.slice(-1)[0]
-      if (components.includes(getInstance(instance))) {
+      if (components.includes(getInstance(component))) {
         modifiedProps = {
           ...modifiedProps,
           ...overrideProps,
@@ -23,6 +23,13 @@ export function useOverrideProps<Props>(instance, props) {
     ...modifiedProps,
     ...props,
   }
+}
+
+export function override<C extends React.ElementType>(
+  component: C,
+  props: React.ComponentProps<C>
+): [C, React.ComponentProps<C>] {
+  return [component, props]
 }
 
 export function Overrides({ value, children }) {
