b7c9ad020475561db4ea4c095b3f7d37b0ea1a57
Travis Arnold <ftntravis@gmail.com>
2020-11-27 17:58:53 -0800

    get simple API working

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,24 +1,78 @@
 import express from 'express'
+import cors from 'cors'
+import * as fs from 'fs/promises'
+import * as prettier from 'prettier'
+import * as codemod from '@codemod/core'
+
 import { makeExpressHandlers } from '../flows/sextant-express.generated.js'
 
 const app = express()
 
+const plugin = () => {
+  return {
+    visitor: {
+      JSXText(path, state) {
+        const location = path.parent.openingElement.loc
+        if (
+          state.opts.source.lineNumber === location.start.line &&
+          state.opts.source.columnNumber === location.start.column + 1
+        ) {
+          path.node.value = path.node.value.replace(
+            path.node.value.trim(),
+            state.opts.value.trim()
+          )
+        }
+      },
+    },
+  }
+}
+
+type ComponentData = {
+  source: {
+    fileName: string,
+    lineNumber: number,
+    columnNumber: number,
+  },
+  value: any,
+}
+
 const handlers = makeExpressHandlers('frontend', 'server', {
   props: {
-    add: (request, response) => {
-      console.log('add', request.query)
-      response.sendStatus(200)
-    },
     update: (request, response) => {
-      console.log('update')
-      response.sendStatus(200)
-    },
-    remove: (request, response) => {
-      console.log('remove')
-      response.sendStatus(200)
+      let bodyBuffer = []
+      request
+        .on('error', (err) => {
+          console.error(err)
+        })
+        .on('data', (chunk) => {
+          bodyBuffer.push(chunk)
+        })
+        .on('end', async () => {
+          const body = JSON.parse(Buffer.concat(bodyBuffer).toString()) as ComponentData
+          const fileBuffer = await fs.readFile(body.source.fileName)
+          const prettierOptions = await prettier.resolveConfig(
+            body.source.fileName
+          )
+          const transformedFile = codemod.transform(fileBuffer.toString(), {
+            plugins: [[plugin, body]],
+          })
+          const formattedCode = prettier.format(
+            transformedFile.code,
+            prettierOptions
+          )
+          try {
+            await fs.writeFile(body.source.fileName, formattedCode)
+            response.sendStatus(200)
+          } catch (err) {
+            response.sendStatus(500)
+          }
+        })
     },
   },
 })
+
+// TODO: add option for specifying origin
+app.use(cors({ origin: true }))
 
 handlers.forEach(({ feature, handler }) => {
   Object.entries(handler).forEach(([scenario, callback]) => {
@@ -26,48 +80,5 @@ handlers.forEach(({ feature, handler }) => {
   })
 })
 
-app.get('/', (request, response) => {
-  response.send('HELLO WORLD')
-})
-
 app.listen(4000)
 
-// import fs from 'fs'
-// import http from 'http'
-
-// http
-//   .createServer((request, response) => {
-//     const { host } = request.headers
-//     let body = []
-//     if (host.startsWith('http://localhost') || host.startsWith('localhost')) {
-//       request
-//         .on('error', (err) => {
-//           console.error(err)
-//         })
-//         .on('data', (chunk) => {
-//           body.push(chunk)
-//         })
-//         .on('end', () => {
-//           const bodyString = JSON.stringify(Buffer.concat(body).toString())
-
-//           response.on('error', (err) => {
-//             console.error(err)
-//           })
-//           console.log(request.headers)
-//           response.writeHead(200, {
-//             // 'Access-Control-Allow-Origin': request.headers.origin,
-//             'Access-Control-Allow-Methods': 'POST',
-//             'Access-Control-Allow-Headers': 'Content-Type',
-//             'Content-Type': 'application/json',
-//           })
-
-//           fs.writeFileSync('data.json', bodyString)
-
-//           response.end(bodyString)
-//         })
-//     } else {
-//       response.writeHead(403)
-//       response.end()
-//     }
-//   })
-//   .listen(8080)
