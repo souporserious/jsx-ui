f65ff35b37cb7143a0893bb3752333a2da88f439
Travis Arnold <ftntravis@gmail.com>
2021-01-28 23:58:02 -0800

    chore(Modifiers): cleanup commented code

--- packages/react/src/Modifiers.tsx
+++ packages/react/src/Modifiers.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react'
-import mergeProps from 'merge-props'
-import dlv from 'dlv'
 
 const ModifiersContext = React.createContext({})
 
 export function useModifierProps({ modifiers = [], ...props }) {
   const contextValue = React.useContext(ModifiersContext)
   let modifierProps = {}
-  console.log(modifiers)
-  // normalize value to always be an array so it's easier to work with
   if (typeof modifiers === 'string') {
     modifiers = [modifiers]
   }
@@ -18,36 +14,18 @@ export function useModifierProps({ modifiers = [], ...props }) {
       ...contextValue[modifier],
     }
   })
-  return mergeProps(props, modifierProps)
+  return { ...modifierProps, ...props }
 }
 
 export function Modifiers({ children, ...props }) {
   const contextModifiers = React.useContext(ModifiersContext)
-  //   const nextModifiers = React.useMemo(() => {
-  //     const collections = { ...props }
-  //     for (let collectionKey in collections) {
-  //       const tokens = collections[collectionKey]
-  //       for (let tokenKey in tokens) {
-  //         const value = tokens[tokenKey]
-  //         const aliasValue = dlv(collections, [collectionKey, value])
-  //         if (aliasValue) {
-  //           collections[collectionKey][tokenKey] = aliasValue
-  //         }
-  //       }
-  //     }
-  //     return collections
-  //   }, [props])
-  //   const contextValue = React.useMemo(
-  //     () => ({
-  //       ...contextModifiers,
-  //       ...nextModifiers,
-  //     }),
-  //     [contextModifiers, nextModifiers]
-  //   )
-  const contextValue = {
-    ...contextModifiers,
-    ...props,
-  }
+  const contextValue = React.useMemo(
+    () => ({
+      ...contextModifiers,
+      ...props,
+    }),
+    [contextModifiers, ...Object.values(props)]
+  )
   return (
     <ModifiersContext.Provider value={contextValue}>
       {children}
